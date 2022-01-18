cc90716769b979853885477dc11292ae82682f98
jin-Pro <70205497+jin-Pro@users.noreply.github.com>
2022-01-18 10:06:22 +0900

    Revert "1/17 PR"

--- public/js/utils/queryparser.js
+++
@@ -1,5 +0,0 @@
-export default function queryStringParser(querystring) {
-    const result = {};
-    querystring.replace(/[?&]{1}([^=&#]+)=([^&#]*)/g, function(s, k, v) { result[k] = decodeURIComponent(v); });
-    return result;
-}
